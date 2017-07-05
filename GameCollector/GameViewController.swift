15383ba3d039031dc08a06e023df716e7ebe644b
Daniel Bonaparte <danielbonaparte@Daniels-MacBook-Pro.local>
2017-07-05 13:17:47 -0700

    We can create game objects and pull them out of core data

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,57 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Daniel Bonaparte on 7/4/17.
+//  Copyright © 2017 bonapARTe. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var gameImageView: UIImageView!
+    @IBOutlet var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+    
+    @IBAction func photosTapped(_ sender: AnyObject) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as!
+            UIImage
+        
+        gameImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let game = Game(context: context)
+        game.title = titleTextField.text
+        game.image = UIImagePNGRepresentation(gameImageView.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+    }
+    
+}
